a7229b29b09441760823e7a29acfb2af2e689085
SoohoPark <clapp301@gmail.com>
2023-06-21 16:42:26 +0900

    회원 리포지토리 테스트 케이스 작성

--- src/main/java/com/spring/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/com/spring/hellospring/repository/MemoryMemberRepository.java
@@ -4,6 +4,10 @@ import com.spring.hellospring.domain.Member;
 
 import java.util.*;
 
+/**
+ * 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
+ */
+
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
@@ -31,5 +35,9 @@ public class MemoryMemberRepository implements MemberRepository {
     @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
     }
 }
